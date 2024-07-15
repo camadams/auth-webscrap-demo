eb095387380cc43ec801ee874a9166c15e17c48a
Cameron Adams <camgadams@gmail.com>
2024-07-15 23:50:30 +0200

    fix: clean up and build fix

--- app/api/user/route.ts
+++
@@ -1,7 +0,0 @@
-// import type { NextApiRequest, NextApiResponse } from "next";
-// import { NextResponse } from "next/server";
-
-// export async function GET(req: NextApiRequest, res: NextApiResponse) {
-
-//   return NextResponse.json(hrefs, { status: 200 });
-// }
